01954f2ede448f67a059c13fa82a1397386e3b8f
Brod8362 <brod8362@gmail.com>
2024-02-20 17:50:57 -0600

    render default placeholder templates in backend

--- src/backend/src/routes.rs
+++ src/backend/src/routes.rs
@@ -1,25 +1,52 @@
+use rocket_dyn_templates::{context, Template};
+
 
 #[get("/")]
-pub fn index() -> String {
-    String::from("hi")
+pub fn index() -> Template {
+    Template::render(
+        "placeholder", 
+        context!{
+            
+        }
+    )
 }
 
 #[get("/login")]
-pub fn login() -> String {
-    String::from("login")
+pub fn login() -> Template {
+    Template::render(
+        "placeholder", 
+        context!{
+            
+        }
+    )
 }
 
 #[get("/register")]
-pub fn register() -> String {
-    String::from("register")
+pub fn register() -> Template {
+    Template::render(
+        "placeholder", 
+        context!{
+            
+        }
+    )
 }
 
 #[get("/page/profile")]
-pub fn profile() -> String {
-    String::from("profile page")
+pub fn profile() -> Template {
+    Template::render(
+        "placeholder", 
+        context!{
+            
+        }
+    )
 }
 
 #[get("/page/quote")]
-pub fn quote() -> String {
-    String::from("quote page")
+pub fn quote() -> Template {
+    Template::render(
+        "placeholder", 
+        context!{
+            
+        }
+    )
 }
