26b3820e90f09dd4cef734b345a967e5ceeab0ad
Brod8362 <brod8362@gmail.com>
2024-02-20 17:43:24 -0600

    Template project for frontend/backend glue

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+pub mod routes;
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount(
+        "/", 
+        routes![
+            routes::index,
+            routes::login,
+            routes::profile,
+            routes::quote
+        ]
+    )
 }
