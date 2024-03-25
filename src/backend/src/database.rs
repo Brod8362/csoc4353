d91ad64ac4e6810a0d491c3d5639609eaa7e61c4
tnttruong <thomasnttruong@gmail.com>
2024-03-25 13:36:16 -0500

    removed future database implementation

--- src/backend/src/database.rs
+++ src/backend/src/database.rs
@@ -128,13 +128,6 @@ pub async fn authenticate_user(pool: &Pool<Sqlite>, username: &str, password: &s
 
 }
 
-/*
-pub async fn store_quote(pool: &Pool<Sqlite>, gallons: &i32, address: &str, date: &str) -> Result<String, Box<dyn Error>>{
-    let rs
-}
-*/
-
-
 mod tests {
     use rocket::tokio;
     #[tokio::test]
